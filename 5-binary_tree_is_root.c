57b2f010d2452e601e259e83afee3102f6ba07ed
LuvliNay <nayshka1206@gmail.com>
2025-04-10 14:26:28 -0400

    task 5 is root

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -10,10 +10,8 @@ int binary_tree_is_root(const binary_tree_t *node)
 {
 	if (node == NULL)
 		return (0);
-	binary_tree_is_root(node->left);
-	binary_tree_is_root(node->right);
 
-	if (node->left != NULL && node->right != NULL)
+	if (node->parent == NULL)
 		return (1);
 	else
 		return (0);
